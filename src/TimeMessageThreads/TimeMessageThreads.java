a05b1665115cdc0f55644d8df395495607d904ab
Anna Motuz <annamotuz510@gmail.com>
2023-08-08 01:56:26 +0300

    Was used \r to move the cursor to the beginning of the line, allowing the previous values to be overwritten.

--- src/TimeMessageThreads/TimeMessageThreads.java
+++ src/TimeMessageThreads/TimeMessageThreads.java
@@ -1,4 +1,5 @@
 package TimeMessageThreads;
+
 import java.util.concurrent.TimeUnit;
 
 public class TimeMessageThreads {
@@ -11,28 +12,31 @@ public class TimeMessageThreads {
         messageThread.start();
     }
 }
+
 class TimeDisplay implements Runnable {
     @Override
     public void run() {
         try {
             int seconds = 0;
             while (true) {
-                System.out.println("Elapsed time: " + seconds + " seconds");
+                System.out.print("Elapsed time: " + seconds + " seconds");
                 seconds++;
                 TimeUnit.SECONDS.sleep(1);
+                System.out.print("\r"); // Move the cursor back to the beginning of the line
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 }
+
 class MessageDisplay implements Runnable {
     @Override
     public void run() {
         try {
             while (true) {
-                System.out.println("Have passed 5 seconds.");
                 TimeUnit.SECONDS.sleep(5);
+                System.out.println("\nHave passed 5 seconds.");
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
